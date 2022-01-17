c2ca16a3a5d3f9a69f8b093c175f971d19f555f5
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-01-17 14:55:42 +0000

    Added some TODOs

--- dist/index.js
+++ dist/index.js
@@ -21606,6 +21606,7 @@ const yaml = __nccwpck_require__(1917);
       const revisions = await Promise.all(
         images.map(async ([resource_name, resource_image]) => {
           await exec.exec('docker', ['pull', resource_image]);
+          // TODO: Check not useful code when not uploading the OCI image
           if(upload_image){
             await exec.exec('charmcraft', [
               'upload-resource',
--- main.js
+++ main.js
@@ -102,6 +102,7 @@ const yaml = require('js-yaml');
       const revisions = await Promise.all(
         images.map(async ([resource_name, resource_image]) => {
           await exec.exec('docker', ['pull', resource_image]);
+          // TODO: Check not useful code when not uploading the OCI image
           if(upload_image){
             await exec.exec('charmcraft', [
               'upload-resource',
