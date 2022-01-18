da949f34088c9339dc1de744becda4dacdc5b29c
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-01-18 11:44:00 +0000

    Changed test-charm

---
+++ test-charm/README.md
@@ -0,0 +1,5 @@
+# edge-release-test-charm
+
+## Description
+
+Dummy charm to test automatic updates to charms on in Charmhub from CI
--- test-charm/src/charm.py
+++ test-charm/src/charm.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-# Copyright 2021 Canonical Ltd.
+# Copyright 2022 Ubuntu
 # See LICENSE file for licensing details.
 
+
 from ops.charm import CharmBase
+from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus
 
+logger = logging.getLogger(__name__)
 
-class Operator(CharmBase):
+
+class EdgeReleaseTestCharmCharm(CharmBase):
+    """Charm the service."""
+
     def __init__(self, *args):
         super().__init__(*args)
-
         self.framework.observe(self.on.install, self.install)
 
     def install(self, event):
@@ -18,4 +23,4 @@ class Operator(CharmBase):
 
 
 if __name__ == "__main__":
-    main(Operator)
+    main(EdgeReleaseTestCharmCharm)
