73822c486d865aac9a25655d607350bac4387c9b
Javier Sánchez Fernández <32944505+sanchezfdezjavier@users.noreply.github.com>
2022-03-22 15:01:19 +0000

    Fix: fails when upload-image:false (#33)
    * Fix: when upload-image: false, the resource flag at the end of charmcraft upload command is empty
    
    * Fix upload-image flag test

--- src/services/charmcraft/charmcraft.test.ts
+++ src/services/charmcraft/charmcraft.test.ts
@@ -97,3 +97,48 @@ describe('the charmcraft service', () => {
     });
   });
 });
+
+describe('the check for upload resources', () => {
+  [
+    {
+      flags: ['edge-release-test-charm-image:17'],
+      resourceInfo: 'resources:\n',
+      images: [
+        [
+          'edge-release-test-charm-image',
+          'docker.artifactory.magmacore.org/controller:latest',
+        ],
+      ],
+    },
+    {
+      flags: ['lollercopter-image:1', 'bueno-image:4'],
+      resourceInfo: 'resources:\n',
+      images: [
+        [
+          'lollercopter-image',
+          'docker.artifactory.magmacore.org/controller:latest',
+        ],
+        ['bueno-image', 'docker.artifactory.magmacore.org/controller:latest'],
+      ],
+    },
+  ].forEach(({ flags, resourceInfo, images }) => {
+    it(`should return the right flags and resource info`, async () => {
+      const charmcraft = new Charmcraft('token');
+
+      charmcraft.uploadResource = jest.fn();
+      charmcraft.metadata = jest.fn(() => ({
+        name: 'hello',
+        files: ['resource_1'],
+        images,
+      }));
+
+      charmcraft.buildResourceFlag = jest.fn(async (_name, resource_name) => ({
+        flag: `${flags.find((f) => f.includes(resource_name))}`,
+        info: ``,
+      }));
+
+      const uploadResources = await charmcraft.uploadResources();
+      expect(uploadResources).toEqual({ flags, resourceInfo });
+    });
+  });
+});
