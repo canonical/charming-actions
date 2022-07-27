a61648f15a6608837c603713d512707517f4cd00
Simon Aronsson <simme@arcticbit.se>
2022-07-27 16:50:24 +0200

    use programmatic output for charmcraft upload (#50)
    * use programmatic output for charmcraft upload
    
    * Update integrate.yaml
    
    * add debug statements
    
    * remove quiet as that silences the json output as well

--- src/services/charmcraft/charmcraft.ts
+++ src/services/charmcraft/charmcraft.ts
@@ -187,14 +187,15 @@ class Charmcraft {
 
     const args = [
       'upload',
-      '--quiet',
+      '--format',
+      'json',
       '--release',
       channel,
       paths[0],
       ...flags,
     ];
     const result = await getExecOutput('charmcraft', args, this.execOptions);
-    const newRevision = result.stdout.split(' ')[1];
+    const newRevision = JSON.parse(result.stdout).revision;
     return newRevision;
   }
 
