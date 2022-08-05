30cd6d61ca597319a1608494a659fbaed9d90976
Simon Aronsson <simme@arcticbit.se>
2022-08-05 14:09:51 +0200

    switch default for check libs to use labels to reduce spam

--- src/actions/check-libraries/check-libraries.ts
+++ src/actions/check-libraries/check-libraries.ts
@@ -31,6 +31,7 @@ export class CheckLibrariesAction {
     this.outcomes = {
       fail: getInput('fail-build').toLowerCase() === 'true',
       comment: getInput('comment-on-pr').toLowerCase() === 'true',
+      labels: getInput('use-labels').toLowerCase() === 'true',
     };
 
     this.context = context;
@@ -48,12 +49,21 @@ export class CheckLibrariesAction {
       }
       await this.snap.install('charmcraft', this.channel);
       const status = await this.charmcraft.hasDriftingLibs();
+
+      if (this.shouldAddLabels) {
+        const success = getInput('label-success');
+        const fail = getInput('label-fail');
+        await this.configureLabels(success, fail);
+        await this.replaceLabel(
+          status.ok ? success : fail,
+          status.ok ? fail : success
+        );
+      }
+
       // we do this using includes to catch both `pull_request` and `pull_request_target`
       if (!status.ok && this.shouldPostComment) {
-        this.github.rest.issues.createComment({
-          issue_number: this.context.issue.number,
-          owner: this.context.repo.owner,
-          repo: this.context.repo.repo,
+        await this.github.rest.issues.createComment({
+          ...this.identifiers,
           body: this.getCommentBody(status, this.charmPath),
         });
       }
@@ -66,6 +76,76 @@ export class CheckLibrariesAction {
       setFailed(e.message);
       error(e.stack);
     }
+  }
+
+  private async configureLabels(success: string, fail: string) {
+    const { owner, repo } = this.identifiers;
+
+    await Promise.all(
+      [
+        { name: success, color: '#00ff00' },
+        { name: fail, color: '#ff0000' },
+      ].map(async (label) => {
+        const identifier = { owner, repo, name: label.name };
+        const response = await this.github.rest.issues.getLabel(identifier);
+        const exists = response.status === 200;
+
+        if (exists) {
+          await this.github.rest.issues.updateLabel({
+            ...identifier,
+            color: label.color,
+          });
+        } else {
+          await this.github.rest.issues.createLabel({
+            ...identifier,
+            color: label.color,
+          });
+        }
+      })
+    );
+  }
+
+  private async replaceLabel(newLabel: string, oldLabel: string) {
+    const labels = (
+      await this.github.rest.issues.listLabelsOnIssue({
+        ...this.identifiers,
+      })
+    ).data;
+
+    const hasOldLabel = labels && labels.find((x) => x.name === oldLabel);
+    const hasNewLabel = labels && labels.find((x) => x.name === newLabel);
+
+    if (!hasNewLabel) {
+      await this.github.rest.issues.addLabels({
+        ...this.identifiers,
+        labels: [newLabel],
+      });
+    }
+
+    if (hasOldLabel) {
+      await this.github.rest.issues.removeLabel({
+        ...this.identifiers,
+        name: oldLabel,
+      });
+    }
+  }
+
+  private get identifiers() {
+    const { owner, repo } = this.context.repo;
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const issue_number = this.context.issue.number;
+    return {
+      owner,
+      repo,
+      issue_number,
+    };
+  }
+
+  private get shouldAddLabels() {
+    return (
+      this.outcomes.labels && this.context.eventName.includes('pull_request')
+    );
   }
 
   private get shouldPostComment() {
