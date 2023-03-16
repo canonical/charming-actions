c015211a4dfeda1469f5b15612b53034c5e23e3f
Simon Aronsson <simme@arcticbit.se>
2023-03-16 09:09:22 +0100

    Update src/services/charmcraft/charmcraft.ts

--- src/services/charmcraft/charmcraft.ts
+++ src/services/charmcraft/charmcraft.ts
@@ -194,7 +194,7 @@ class Charmcraft {
     const paths = await globber.glob();
 
     // filter all characters which are not letters, numbers or hyphens
-    const allowedChannel = channel.replace(/[^a-zA-Z0-9\-\/]/gi, '');
+    const allowedChannel = channel.replace(/[^a-zA-Z0-9\-/]/gi, '');
     const args = [
       'upload',
       '--format',
