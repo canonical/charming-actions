2f8c775faebe7a413adc5bb80d49cfc8487fb737
Simon Aronsson <simme@arcticbit.se>
2023-09-27 13:58:33 +0200

    fail channel action if branch name is invalid

--- src/actions/channel/channel.ts
+++ src/actions/channel/channel.ts
@@ -1,4 +1,4 @@
-import { setOutput } from '@actions/core';
+import { setOutput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 import { Context } from '@actions/github/lib/context';
 import { Ref } from '../../services';
@@ -13,7 +13,12 @@ export class ChannelSelection {
   }
 
   run() {
-    const channel = this.ref.channel();
+    try {
+      const channel = this.ref.channel();
+    } catch (error) {
+      setFailed(error.message);
+    }
+
     setOutput('name', channel);
   }
 }
