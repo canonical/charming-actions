934193396735701141a1decc3613818e412da606
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2024-08-29 13:36:12 +0300

    fix(upload-bundle): Implement the same functionality without using `juju-bundle` (#151)
    * Update upload-bundle action
    
    * Update upload-bundle action
    
    * Update upload-bundle action
    
    * Update upload-bundle action
    
    * Update upload-bundle action
    
    * Update if-else blocks to be more legible
    
    * Update bundle.ts to use inverted conditionals

--- src/services/bundle/bundle.ts
+++ src/services/bundle/bundle.ts
@@ -1,17 +1,25 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import { exportVariable, getInput } from '@actions/core';
+import { exec, getExecOutput } from '@actions/exec';
 
 class Bundle {
   async publish(path: string, channel: string) {
-    core.exportVariable('CHARMCRAFT_AUTH', core.getInput('credentials'));
+    exportVariable('CHARMCRAFT_AUTH', getInput('credentials'));
     process.chdir(path);
-    await exec.exec('juju-bundle', [
-      'publish',
-      '--destructive-mode',
-      '--serial',
-      '--release',
-      channel,
-    ]);
+
+    const result = await getExecOutput('charmcraft', ['pack']);
+    if (result.exitCode !== 0) {
+      throw new Error(
+        `charmcraft pack ran unsuccessfully with exit code ${result.exitCode}.`,
+      );
+    }
+    const lastLine = result.stderr.trim().split('\n').pop();
+    if (!lastLine) {
+      throw new Error(
+        'Failed to extract the bundle name from the output of charmcraft pack.',
+      );
+    }
+    const bundleName = lastLine.split(' ')[1];
+    await exec('charmcraft', ['upload', bundleName, '--release', channel]);
   }
 }
 
